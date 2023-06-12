75d868f5aca498f95240c51e612482ab4ef101c7
Tomas662 <131295857+Tomas662@users.noreply.github.com>
2023-06-12 20:35:31 +0200

    Update t_tomas_marek_project_SQL_secondary_final.sql

--- t_tomas_marek_project_SQL_secondary_final.sql
+++ t_tomas_marek_project_SQL_secondary_final.sql
@@ -1,18 +1,13 @@
 
 CREATE TABLE t_tomas_marek_project_sql_secondary_final AS
 	SELECT
-		country.country,
-		country.continent,
-		country.population,
+		countries.country,
+		countries.continent,
+		countries.population,
 		economy.YEAR,
 		economy.GDP,
 		economy.gini
-	FROM
-		(SELECT
-			country,
-		 	continent,
-		 	population
-		FROM countries) country 
+	FROM countries
 	JOIN 
 		(SELECT 
 			YEAR,
@@ -21,6 +16,6 @@ CREATE TABLE t_tomas_marek_project_sql_secondary_final AS
 		 	country
 		FROM economies
 		WHERE YEAR BETWEEN 2006 AND 2018) economy  
-	ON country.country = economy.country
+	ON countries.country = economy.country
 	WHERE continent = 'Europe'	
-GROUP BY country.country, economy.YEAR DESC;
+GROUP BY countries.country, economy.YEAR DESC;
